6efaf7d1314b15927e15de34a65f7096403d3486
KJH-x <53437291+KJH-x@users.noreply.github.com>
2026-08-10 11:57:22 +0800

    feat: themed accessible modal dialogs replacing confirm/prompt

--- test.py
+++ test.py
@@ -53,6 +53,8 @@ JS_REQUIRED_PATTERNS = {
     "IIFE wrapper": r"\(function\s*\(\)",
     "use strict": r"'use strict'",
     "md-pagewidth localStorage": r"md-pagewidth",
+    "MDModal confirm": r"MDModal\.confirm",
+    "MDModal prompt": r"MDModal\.prompt",
     "file-io sanitize": r"sanitizeFilename",
     "file-io decode": r"decodeFile",
 }
